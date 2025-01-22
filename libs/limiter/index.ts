851f99794b86c0f8434af37c36ec15cbfb63834c
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2025-01-22 14:34:29 +0300

    fix: rate limit increased

--- libs/limiter/index.ts
+++ libs/limiter/index.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 import { Request, Response } from 'express';
 
 const RATE_LIMIT_WINDOW_MS = process.env.RATE_LIMIT_WINDOW_MS || 15 * 60 * 1000;
-const RATE_LIMIT_MAX = process.env.RATE_LIMIT_MAX || 2;
+const RATE_LIMIT_MAX = process.env.RATE_LIMIT_MAX || 100;
 
 export default class Limiter {
 	static limiter = rateLimit({
