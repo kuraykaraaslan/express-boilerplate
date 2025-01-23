13abea2e134293e185fd0ca58099deee9e0214bc
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2025-01-23 10:47:15 +0300

    feat: relative paths disabled

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import logger from 'morgan';
-import ErrorHandler from "@/middlewares/ErrorHandler";
+import ErrorHandler from "./middlewares/ErrorHandler";
 
 import dotenv from "dotenv";
 import IndexRouter from "./routers";
