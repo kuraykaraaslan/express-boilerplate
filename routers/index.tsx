349071d58bc8b90be0a19b85dc74d9671ffe3da6
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2025-01-29 09:31:01 +0300

    feat: 404 handler

--- routers/index.tsx
+++ routers/index.tsx
@@ -14,10 +14,7 @@ IndexRouter.use(Logger.useLogger);
 IndexRouter.use(Limiter.useLimiter);
 
 IndexRouter.get("/", (req, res) => {
-
-
     res.setHeader("Content-Type", "application/json");
-
     return res.send({
         message: "Welcome to the Express Boilerplate",
         version: "2.0.0",
@@ -31,5 +28,12 @@ IndexRouter.get("/", (req, res) => {
 
 IndexRouter.use("/api/v1", V1Router);
 IndexRouter.use("/views", ViewRouter);
+
+IndexRouter.use((req, res) => {
+    res.status(404).send({
+        error: "NOT_FOUND",
+        status: 404
+    });
+});
 
 export default IndexRouter;
